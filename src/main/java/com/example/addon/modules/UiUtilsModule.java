5041f9072e9de616d016f63a1de7ba4fe4bfd778
PIGY999 <pedrofermag@gmail.com>
2026-03-25 17:43:38 +0100

    fixed readme

--- src/main/java/com/example/addon/modules/UiUtilsModule.java
+++ src/main/java/com/example/addon/modules/UiUtilsModule.java
@@ -1,12 +1,12 @@
 package com.example.addon.modules;
 
-import com.example.addon.AddonTemplate;
+import com.example.addon.UIUtils;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 
 public class UiUtilsModule extends Module {
     public UiUtilsModule() {
-        super(AddonTemplate.CATEGORY, "ui-utils", "Enables Meteor UI utility overlays on GUIs.");
+        super(UIUtils.CATEGORY, "ui-utils", "Enables Meteor UI utility overlays on GUIs.");
     }
 
     public static boolean isEnabled() {
